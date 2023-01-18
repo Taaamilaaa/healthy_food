d483991bea50a215166feff0ac1cda34e7f96f55
Yudina Tamila <79252896+Taaamilaaa@users.noreply.github.com>
2023-01-18 16:02:18 +0200

    Create refs.js

---
+++ js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+    sliderBackBtn: document.querySelector('.slider-btn__left'),
+    sliderForwardBtn: document.querySelector('.slider-btn__right'),
+    sliderCont_1: document.querySelector(".slider-item__container_1"),
+    sliderCont_2: document.querySelector(".slider-item__container_2"),
+
+}
